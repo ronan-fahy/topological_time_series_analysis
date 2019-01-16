50f9c78a789dabb5fb7df45af85de3b2bb916d90
Ronan Fahy <r.fahy6@nuigalway.ie>
2019-01-16 14:21:59 +0000

    Made some modifications so that an average time is associated with each point in embedding

--- topological_time_series_analysis.py
+++ topological_time_series_analysis.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from euclidean_metric import EuclideanMetric
 import math
 
 # ****************************************************
@@ -9,37 +8,31 @@ import math
 # S:    time-series in dictionary form
 # d:    dimension of embedding
 # tau:  delay, how far back in the time-series to look
-# T:    time-series in form of list of lists
+# T:    Takens embedding in form of dictionary
 # ****************************************************
 def Takens(S, d, tau):
-   
-    if d < 1:
-        print("d must be greater than or equal to 1")
-        return
-
-    if tau < 1:
-        print("tau must be greater than or equal to 1")
-        return
-
-    T = []
+    T = {}
     for i, (t, value) in enumerate(S.items()):
         if (i+1)%tau == 0:
+            mean_time = 0
             temp = []
             for j in range(0, d):
+                mean_time = mean_time + t
                 try:
-                    temp = [S.get(t-((j*tau)/d))] + temp
+                    temp = [S.get(int(max(0, round(t-((j*tau)/d)))))] + temp
                 except TypeError:
                     print("d and tau must be integers greater than one")
-            T.append(temp)
+            mean_time = mean_time / d
+            T[mean_time] = temp
             
     return T
 
-# ********************************
+# ****************************************************
 # Compute distance between v and w
 #
-# v: 
-# w: 
-# ********************************
+# v: point in embedding
+# w: point in embedding
+# ****************************************************
 def EuclideanMetric(v, w):
     sum = 0
     for a, b in zip(v, w):
@@ -47,17 +40,26 @@ def EuclideanMetric(v, w):
 
     return math.sqrt(sum)
 
-# ***************************************************
+# ****************************************************
 # Get neighbourhood of v in T
 # 
-# T:       time-series in form of list of lists
+# T:       Takens embedding in form of dictionary
 # v:       vector from T, list
 # d:       dimension of embedding
 # epsilon: nearness threshold
 # x:       subsequence of T in the neighbourhood of v
-# ***************************************************
+# ****************************************************
 def Neighbourhood(T, v, d, epsilon):
-    for y in T:
-        if EuclideanMetric(y, v) < epsilon:
-            x.append(y)
+    x = {}
+    for t, w in T.items():
+        if EuclideanMetric(v, w) < epsilon:
+            x[t] = w
     return x
+
+# ****************************************************
+# Get some notion of time for each point in emedding
+#
+# w: point for which time must be gotten
+# ****************************************************
+#def t(w):
+    
