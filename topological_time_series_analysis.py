f3995475212b826392caeb9bcc0b0f91e27a94d9
Ronan Fahy <r.fahy6@nuigalway.ie>
2019-01-16 17:20:20 +0000

    Moved functions from tides_period_2.py

--- topological_time_series_analysis.py
+++ topological_time_series_analysis.py
@@ -1,51 +1,136 @@
 #!/usr/bin/python3
 
-import math
+import math, csv
+
+# Read CSV file into 1-dimensional time-series
+#
+# filepath: path to CSV file to be read from
+# n:        number of rows to read
+# Y:        returned time-series
+def csv_to_time_series(filepath, n):
+    with open(filepath) as csvfile:
+        reader = csv.reader(csvfile)
+        Y = {}
+        for row in reader:
+            t, y = row
+            # divide by 6 to get times in units of 6 minutes
+            Y[int(int(t)/6)] = float(y)
+    Y = {k: Y[k] for k in list(Y)[:n]}
+    return Y
+
+# Filter based on threshold
+#
+# h:        return only entries with value greater than h
+# Y:        time-series to be filtered
+def threshold(h, Y):
+    return {t : Y[t] for t in Y if Y[t] > h}
+
+# Create clusters
+#
+# times:    
+# epsilon:  
+def create_clusters(times, epsilon):
+    clusters = [[]]
+    prev_t = times[0]
+    cluster_idx = 0
+    clusters[cluster_idx] = []
+    for t in times:
+        if (abs(t - prev_t)) < epsilon:
+            clusters[cluster_idx].append(t)
+        else:
+            cluster_idx += 1
+            clusters.append([t])
+        prev_t = t
+    return clusters
+
+# Calculate averages
+#
+# clusters: 
+def calculate_averages(clusters):
+    averages = []
+    for cluster in clusters:
+        total = 0
+        for t in cluster:
+            total += t
+        averages.append(total/len(cluster))
+    return averages
+
+# Calculate differences
+#
+# averages: 
+def calculate_differences(averages):
+    differences = []
+    prev_average = averages[0]
+    for average in averages[1:]:
+        differences.append(average - prev_average)
+        prev_average = average
+    return differences
 
 # ****************************************************
 # Create takens embedding
 #
-# S:    time-series in dictionary form
-# d:    dimension of embedding
+# S:    time-series in dictionary form with time as
+#       key
+# d:    dimension of Takens embedding
 # tau:  delay, how far back in the time-series to look
-# T:    Takens embedding in form of dictionary
+# T:    Takens embedding in form of dictionary with
+#       mean time as key and vector as value
 # ****************************************************
 def Takens(S, d, tau):
     T = {}
     for i, (t, value) in enumerate(S.items()):
-        if (i+1)%tau == 0:
+        if i%tau == 0 and i != 0:
             mean_time = 0
             temp = []
             for j in range(0, d):
-                mean_time = mean_time + t
                 try:
-                    temp = [S.get(int(max(0, round(t-((j*tau)/d)))))] + temp
+                    idx = int(round(t-((j*tau)/d)))
+                    temp = [S.get(idx)] + temp
+                    mean_time = mean_time + idx
                 except TypeError:
                     print("d and tau must be integers greater than one")
             mean_time = mean_time / d
             T[mean_time] = temp
-            
+
     return T
 
 # ****************************************************
-# Compute distance between v and w
+# Compute Euclidean distance between v and w
 #
-# v: point in embedding
-# w: point in embedding
+# v: vector
+# w: vector
+# m: calculated distance between v and w
 # ****************************************************
 def EuclideanMetric(v, w):
-    sum = 0
+    if len(v) != len(w):
+        raise ValueError('v and w must have equal length')
+    m = 0
     for a, b in zip(v, w):
-        sum += (a - b) ** 2
+        m = m + (a - b) ** 2
+    return math.sqrt(m)
 
-    return math.sqrt(sum)
+# ****************************************************
+# Compute taxicab distance between v and w
+#
+# v: vector
+# w: vector
+# m: calculated distance between v and w
+# ****************************************************
+def TaxicabMetric(v, w):
+    if len(v) != len(w):
+        raise ValueError('v and w must have equal length')
+    m = 0
+    for a, b in zip(v, w):
+        m = m + abs(a - b)
+    return m
 
 # ****************************************************
 # Get neighbourhood of v in T
 # 
-# T:       Takens embedding in form of dictionary
-# v:       vector from T, list
-# d:       dimension of embedding
+# T:       Takens embedding in form of dictionary with
+#          mean time as key and vector as value
+# v:       vector from T
+# d:       dimension of Takens embedding
 # epsilon: nearness threshold
 # x:       subsequence of T in the neighbourhood of v
 # ****************************************************
@@ -55,11 +140,3 @@ def Neighbourhood(T, v, d, epsilon):
         if EuclideanMetric(v, w) < epsilon:
             x[t] = w
     return x
-
-# ****************************************************
-# Get some notion of time for each point in emedding
-#
-# w: point for which time must be gotten
-# ****************************************************
-#def t(w):
-    
